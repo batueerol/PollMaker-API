eee200bbb991f5c56201a7996dca21b0f46d0443
batueerol <batueerol@gmail.com>
2022-12-17 23:04:51 +0300

    project created.

---
+++ src/main/java/com/example/pollmaker/PollmakerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pollmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollmakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pollmaker/PollmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pollmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
