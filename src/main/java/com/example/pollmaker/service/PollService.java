4e5f00bcea4f7a73699f6ccb6624b434358d3ea1
batueerol <batueerol@gmail.com>
2022-12-26 22:20:45 +0300

    PollService added

--- src/main/java/com/example/pollmaker/service/PollService.java
+++ src/main/java/com/example/pollmaker/service/PollService.java
@@ -3,6 +3,9 @@ package com.example.pollmaker.service;
 import com.example.pollmaker.model.dto.PollDTO;
 import com.example.pollmaker.model.poll.CreatePollRequest;
 
+import java.util.List;
+
 public interface PollService {
     public PollDTO createPoll(CreatePollRequest createPollRequest);
+    public List<PollDTO> getPolls();
 }
--- src/main/java/com/example/pollmaker/service/impl/PollServiceImpl.java
+++ src/main/java/com/example/pollmaker/service/impl/PollServiceImpl.java
@@ -1,14 +1,43 @@
 package com.example.pollmaker.service.impl;
 
+import com.example.pollmaker.domain.Poll;
+import com.example.pollmaker.mapper.PollMapper;
 import com.example.pollmaker.model.dto.PollDTO;
 import com.example.pollmaker.model.poll.CreatePollRequest;
+import com.example.pollmaker.repository.PollRepository;
 import com.example.pollmaker.service.PollService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class PollServiceImpl implements PollService {
+
+
+    private final PollMapper pollMapper;
+
+    private final PollRepository pollRepository;
+
     @Override
     public PollDTO createPoll(CreatePollRequest createPollRequest) {
-        return null;
+        Poll poll = pollMapper.CreatePollRequestToPoll(createPollRequest);
+        poll = pollRepository.save(poll);
+
+        return pollMapper.PollToCreatePollDto(poll);
+    }
+
+    @Override
+    public List<PollDTO> getPolls() {
+        List<Poll> polls = pollRepository.findAll();
+
+        List<PollDTO> pollDTOS = new ArrayList<>();
+
+        for (Poll poll: polls) {
+           pollDTOS.add(pollMapper.PollToCreatePollDto(poll));
+        }
+        return pollDTOS;
     }
 }
