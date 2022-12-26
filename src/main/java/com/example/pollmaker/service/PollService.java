757decc95402aec33b55224416495ed487652dab
batueerol <batueerol@gmail.com>
2022-12-26 17:08:57 +0300

    createPoll method added into PollService

--- src/main/java/com/example/pollmaker/service/PollService.java
+++ src/main/java/com/example/pollmaker/service/PollService.java
@@ -1,4 +1,8 @@
 package com.example.pollmaker.service;
 
+import com.example.pollmaker.model.dto.PollDTO;
+import com.example.pollmaker.model.poll.CreatePollRequest;
+
 public interface PollService {
+    public PollDTO createPoll(CreatePollRequest createPollRequest);
 }
--- src/main/java/com/example/pollmaker/service/impl/PollServiceImpl.java
+++ src/main/java/com/example/pollmaker/service/impl/PollServiceImpl.java
@@ -1,7 +1,14 @@
 package com.example.pollmaker.service.impl;
 
+import com.example.pollmaker.model.dto.PollDTO;
+import com.example.pollmaker.model.poll.CreatePollRequest;
+import com.example.pollmaker.service.PollService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PollServiceImpl {
+public class PollServiceImpl implements PollService {
+    @Override
+    public PollDTO createPoll(CreatePollRequest createPollRequest) {
+        return null;
+    }
 }
