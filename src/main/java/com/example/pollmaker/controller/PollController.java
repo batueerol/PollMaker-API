3227dea493eade3884babe87b43b83df0c4d867c
batueerol <batueerol@gmail.com>
2022-12-26 17:12:33 +0300

    createPoll post method added into PollController

--- src/main/java/com/example/pollmaker/controller/PollController.java
+++ src/main/java/com/example/pollmaker/controller/PollController.java
@@ -1,4 +1,28 @@
 package com.example.pollmaker.controller;
 
+
+import com.example.pollmaker.model.dto.PollDTO;
+import com.example.pollmaker.model.poll.CreatePollRequest;
+import com.example.pollmaker.service.PollService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/poll")
 public class PollController {
+
+    private final PollService pollService;
+
+    @PostMapping("/create-poll")
+    public ResponseEntity<PollDTO> createPoll(@RequestBody CreatePollRequest createPollRequest){
+        PollDTO pollDTO = pollService.createPoll(createPollRequest);
+        return new ResponseEntity(pollDTO, HttpStatus.OK);
+    }
 }
