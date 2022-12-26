6d0bb9f06dc33dcd6247411a14c21fbde0af82ca
batueerol <batueerol@gmail.com>
2022-12-26 22:25:07 +0300

    refactor application.yml, pom.xml, PollmakerApplication.java

--- src/main/java/com/example/pollmaker/PollmakerApplication.java
+++ src/main/java/com/example/pollmaker/PollmakerApplication.java
@@ -2,8 +2,13 @@ package com.example.pollmaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoAuditing //for @CreatedDate annotation
 public class PollmakerApplication {
 
 	public static void main(String[] args) {
