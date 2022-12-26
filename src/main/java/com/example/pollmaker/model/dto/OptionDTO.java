91a4a254aacf6d9cf2b8c6f3cc972c2c0190b075
batueerol <batueerol@gmail.com>
2022-12-26 22:19:22 +0300

    OptionDTO added

---
+++ src/main/java/com/example/pollmaker/model/dto/OptionDTO.java
@@ -0,0 +1,18 @@
+package com.example.pollmaker.model.dto;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OptionDTO {
+    @JsonSerialize(using = ToStringSerializer.class)
+    private String id;
+    String value;
+}
